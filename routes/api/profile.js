68e48c2fc7adccd6c2b2971f193f4d77ec3551eb
Ayan Adak <aadak@vmware.com>
2021-07-19 20:32:44 +0530

    user registration

---
+++ routes/api/profile.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/profiles
+// @desc  Test route
+// @access Public
+
+router.get('/', (req, res)=> {
+    res.send('profiles route');
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db')
+const userRouter = require('./routes/api/user');
+const authRouter = require('./routes/api/auth');
+const postsRouter = require('./routes/api/post')
+const profileRouter = require('./routes/api/profile')
 
 const app = express();
+// connect to database
+connectDB();
 
 const PORT = process.env | 5000;
 
+app.use(express.json({extended : false}));
 app.get('/', (req, res)=> {
     res.send("welcome");
 })
+
+app.use('/api/users',userRouter);
+app.use('/api/profiles',profileRouter);
+app.use('/api/auth',authRouter);
+app.use('/api/posts',postsRouter);
+
 app.listen(PORT, ()=> { console.log(`node started ${PORT}`)})
