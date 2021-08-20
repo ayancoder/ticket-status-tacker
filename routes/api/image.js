88bcf2f95141c3d87f03eebe95111ecd64753c0d
Ayan Adak <aadak@vmware.com>
2021-08-20 22:20:58 +0530

    make uploads folder publicly available

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const { route } = require("./ticket");
 
 const fileFilter = (req, file, callback) => {
   if(file.mimetype === 'image/jpeg' | 
@@ -51,20 +52,22 @@ router.post('/upload', (req, res) => {
   
         } else {
   
-            let path = `/images/${req.file.filename}`
+            let path = `/uploads/${req.file.filename}`
             res.status(200).json({message: 'Image Uploaded Successfully !', path: path})
         }
     })
   })
 
-router.get('/images/:imagename', (req, res) => {
-
+  
+/* router.get('/images/:imagename', (req, res) => {
+    console.log("get image ", imagename);
     let imagename = req.params.imagename
-    let imagepath = __dirname + "/images/" + imagename
+    let imagepath = __dirname + "/uploads/" + imagename
     let image = fs.readFileSync(imagepath)
     let mime = fileType(image).mime
 
 	res.writeHead(200, {'Content-Type': mime })
 	res.end(image, 'binary')
-})
+}); */
+
 module.exports = router;
