6a811c9bae544f8ef0ab1f5fce5936da23f301c9
Ayan Adak <ayan.reader@gmail.com>
2021-09-25 19:48:46 +0530

    pdf and multiple file upload

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,10 +1,14 @@
+const { json } = require("express");
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+var fs = require('fs');
 
 const fileFilter = (req, file, callback) => {
-  if(file.mimetype === 'image/jpeg' | 
-    file.mimetype === 'image/png'){
+ console.log("file -->", file)
+  if(file.mimetype === 'image/jpeg' || 
+    file.mimetype === 'image/png'||
+    file.mimetype == 'application/pdf'){
       callback(null, true);
     }else{ 
       callback(new Error("invalid file extension"), false);
@@ -13,35 +17,70 @@ const fileFilter = (req, file, callback) => {
 }
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "./uploads");
+    const dir = "./uploads/"+getDate();
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir, { recursive: true });
+    } 
+    callback(null, dir);
   },
   filename: function (req, file, callback) {
-    callback(null, new Date().toISOString() + file.originalname);
+    callback(null, new Date().toISOString() +"-" +file.originalname);
   },
 });
+
+const getDate = () => {
+  const event = new Date();
+  const mon = event.getMonth() + 1;
+  const day = event.getDate();
+  const year = event.getFullYear();
+  const dateStr = day + "-" + mon + "-" + year;
+  console.log("getTime", getTime());
+  return dateStr;
+};
+const getTime =() =>{
+
+  const event = new Date();
+  const hour = event.getHours() ;
+  const min = event.getMinutes();
+  const sec = event.getSeconds();
+  const timeStr = hour + "-" + min + "-" + sec;
+  return timeStr;
+}
 const upload = multer({
   storage: storage,
   limits: {
     fileSize: 1024 * 1024 * 5,
   },
   fileFilter: fileFilter,
-}).single('image'); 
+}).array('image'); 
+
   
-router.post('/upload', (req, res) => {
-  
-    upload(req, res, function (err) {
-  
-        if (err) {
-  
-            res.status(400).json({message: err.message})
-  
+router.post("/upload", (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      res.status(400).json({ message: err.message });
+    } else {
+      //console.log("req", req);
+      let pdfFilePath = [];
+      let imgFilePath = [];
+      req.files.forEach((file) => {
+        if (file.mimetype == "application/pdf") {
+          pdfFilePath.push(file.path);
         } else {
-  
-            let path = `/uploads/${req.file.filename}`
-            res.status(200).json({message: 'Image Uploaded Successfully !', path: path})
+          imgFilePath.push(file.path);
         }
-    })
-  })
+      });
+      console.log("files ", pdfFilePath, imgFilePath );
+      res
+        .status(200)
+        .json({
+          message: "Image Uploaded Successfully !",
+          pdf: pdfFilePath,
+          img: imgFilePath,
+        });
+    }
+  });
+});
 
   
 module.exports = router;
